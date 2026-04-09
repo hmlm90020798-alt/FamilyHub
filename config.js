71f202fe2c791c1a56f08d4369355e86fbdbbcb3
hmlm90020798-alt <hmlm.90020798@gmail.com>
2026-04-09 23:58:27 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCyzOg6uP27icPtTvrcBYVE_WZlpWr6Ow4",
+  authDomain: "familyhub-m.firebaseapp.com",
+  projectId: "familyhub-m",
+  storageBucket: "familyhub-m.firebasestorage.app",
+  messagingSenderId: "789023063066",
+  appId: "1:789023063066:web:1cc0bcbeb85a1dd6cd65a3",
+  measurementId: "G-XX3G0SBLWN"
+};
